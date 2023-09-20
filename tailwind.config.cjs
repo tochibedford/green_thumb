6562a413c0c1f0c659fe5a9fb454b59a79c28ec9
tochibedford <musicofbyte@gmail.com>
2023-09-20 09:05:21 +0100

    feat: containerize whole app + desktop first

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,18 @@ module.exports = {
       gPrimaryLight: "#078A6C",
       gPrimary: "#007157",
     },
+    screens: {
+      //switch to desktop first
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
   },
   plugins: [],
 };
