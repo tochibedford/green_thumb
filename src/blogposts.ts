db0efa23e89b1a057c2113ebba93d2ae80dfed8e
tochibedford <musicofbyte@gmail.com>
2023-09-20 18:15:28 +0100

    feat: added blog posts

---
+++ src/blogposts.ts
@@ -0,0 +1,73 @@
+const blogPosts = [
+  {
+    postId: "plant_info",
+    title: "Plant Information and Care",
+    date: "20/09/2023",
+    headerImage:
+      "https://images.pexels.com/photos/18291871/pexels-photo-18291871/free-photo-of-photo-of-a-lily-flower.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    content: `Rosemary holds a special place among herbs. It's loved for its delightful taste and cherished for the many ways it benefits our health. This herb has quite a remarkable history, and its popularity spans centuries. Beyond adding flavor to our dishes, rosemary has been known to contribute to our well-being in various ways. It's not just a simple herb; it's a symbol of remembrance, making it all the more special. So, whether you're seasoning your food or exploring its rich heritage, rosemary has much to offer, both in taste and its fascinating role in our health and history. Small Tip: To ensure rosemary seeds have the best chance of doing well, it's a good idea to begin growing them indoors. If you live in a place with a tropical climate like Singapore, you can grow rosemary all year round. But for those in other areas, because rosemary seeds take their time to sprout and grow, it's best to start them three to six months before the growing season begins. Rosemary often has a reputation for being tricky to grow from seeds. But the secret lies in giving these seeds a little push to help them start growing.
+      There are easy ways to improve the chances of rosemary seeds sprouting. One simple way is to soak the seeds before planting. Just put the rosemary seeds in water and let them sit overnight. This helps get them ready to grow.
+      Another useful method is called “cold stratification.” It means putting the seeds in a cold and damp place for about four weeks. This tricks the seeds into thinking it's time to sprout when you plant them.
+      Another method for those who like to plan ahead is “winter sowing.” It's simple, but it does require some thinking ahead. With winter sowing, you plant rosemary seeds in containers outside during the winter months.
+      After using the soaking or cold stratification methods, your rosemary seeds will be ready for planting.
+      When it's time to plant, you can start rosemary seeds indoors about 10 to 12 weeks before the last expected frost in your area. But because it takes nearly a year for rosemary plants to fully grow, it's a good idea to start the seeds even earlier. This way, the seedlings will have more time to grow indoors before you move them to your garden once the last spring frost has passed.
+      Using these methods to wake up the rosemary seeds and being patient as they grow, you can successfully grow this fragrant herb from seeds. And the joy of having your own rosemary plant will make your cooking even more flavorful.`,
+    otherImages: [
+      "https://images.pexels.com/photos/305087/pexels-photo-305087.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      "https://images.pexels.com/photos/206847/pexels-photo-206847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    postId: "landscaping",
+    title: "Landscaping",
+    date: "20/09/2023",
+    headerImage:
+      "https://images.pexels.com/photos/206847/pexels-photo-206847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    content: `This guide'll walk you through a curated selection of the finest seed starting bundles in 2023. These kits cater to various gardening preferences and needs, ensuring you get a head start on a vibrant and fruitful growing season.
+        From the simplest setups for beginners to advanced systems for seasoned horticulturists, we've handpicked the best options to help you kickstart your garden with confidence. Whether you dream of bountiful vegetable harvests, vibrant flower beds, or lush indoor greenery, our top 7 recommendations have you covered.
+        So don't worry! By the end of this article, you'll be able to select the best seed-strating tray for your garden. So let's get started!
+        The Gardzen Propagator Set is a real game-changer. It includes five seed trays, each boasting 40 cells with drainage holes. Alongside these are five base trays to collect excess water and a transparent dome that securely fits over the trays to maintain the perfect humidity level, creating an ideal environment for seeds to sprout and thrive.
+        What's particularly convenient is that everything I needed came bundled in the set, and the price tag is quite budget-friendly. However, there's a minor drawback worth noting – the seed cells are on the smaller side. As a result, you'll eventually need to transplant your seedlings into larger cells or individual pots before they're ready for outdoor planting.
+        But if you plan to grow many plants, these seed trays are your best bet. Each pack allows you to start a whopping 200 seeds, which translates to a remarkable number of plants! Gardzen Seed Trays are especially well-suited for nurturing various crops, including tomatoes, cucumbers, melons, pumpkins, and more.
+
+        The MIXC Seed Starting Tray is a fantastic choice for anyone eager to kickstart their gardening journey. With 120 cells, this kit offers ample space for nurturing various plants, including small flowers, veggies, fruits, and more.
+        What sets this kit apart is its clear plastic trays, which provide a window into your plants' progress without needing to disturb them. The 1.5-inch dimensions of these seed trays make them perfect for smaller seeds, ensuring your tiny green sprouts get the cozy start they deserve.
+        But that's not all - the MIXC Seed Starting Tray goes the extra mile with adjustable vents in the seed trays. These vents let you take the reins on the temperature and humidity levels within the nurturing environment. Complete control over the growing process? Yes, please!
+        This kit has humidity domes to further sweeten the deal, creating the perfect haven for your seedlings. They shield your budding plants from extreme temperature swings, fostering their growth and protection. Plus, the risk of root oversaturation is greatly diminished due to the thoughtful inclusion of drain holes in each cell.
+        `,
+    otherImages: [
+      "https://images.pexels.com/photos/305087/pexels-photo-305087.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      "https://images.pexels.com/photos/206847/pexels-photo-206847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+  {
+    postId: "gardening",
+    title: "Gardening: Taking care of Leca",
+    date: "20/09/2023",
+    headerImage:
+      "https://images.pexels.com/photos/305087/pexels-photo-305087.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    content: `
+<strong>Why does Leca have algae?</strong>
+
+Leca can sometimes develop algae growth due to its porous nature and ability to retain moisture. Algae thrive in environments with excess moisture, sunlight, and organic matter, and these conditions can occasionally occur within the tiny air pockets and crevices of the Leca clay balls.
+Algae spores can find their way into the Leca substrate through various sources, such as the surrounding air or water used for irrigation. Once they settle in the moist environment of the Leca, they can quickly multiply and form visible colonies of algae.
+Leca, short for Lightweight Expanded Clay Aggregate, is a fascinating material composed of clay balls that undergo a baking process, causing them to expand. Imagine them as clay popcorn or Aero Bar chocolate, except instead of indulging your taste buds, they serve as a perfect substrate for your plants. Leca possesses a unique characteristic – it has a highly porous texture filled with countless tiny air pockets that roots absolutely adore.
+
+What makes Leca even more intriguing is its ability to absorb water. When you water your plants, these clay balls soak up the moisture and expand slightly, ensuring that they retain their porous structure. This super absorbent quality is particularly beneficial for plants that dislike excessive watering. In fact, Leca has the remarkable capacity to absorb around 30% of its own weight in water, making it an excellent choice for those seeking a substrate that helps prevent over-watering.
+The porous texture and tiny air pockets within Leca provide numerous advantages for plant growth. Firstly, they facilitate aeration, ensuring that the roots receive a sufficient oxygen supply. Adequate oxygen levels in the root zone are crucial for healthy plant development. Secondly, the air pockets also assist in drainage, preventing waterlogging and root rot issues that can arise from overly saturated soil.
+Furthermore, Leca offers exceptional insulation properties. It helps maintain stable root temperatures by retaining heat during colder periods and providing a cooling effect during hotter seasons. This insulation capability contributes to a more stable and favorable environment for root growth, supporting overall plant health.
+Another advantage of Leca is its inert nature. Unlike some other substrates, it does not release any harmful chemicals or alter the pH of the soil. This makes it a safe and reliable choice for a wide range of plants.
+
+<strong>Preserving the Benefits of Bacteria</strong>
+If you belong to the group of gardeners who prioritize the preservation of beneficial bacteria and prefer not to use boiling methods, there are alternative approaches for you. Begin by thoroughly cleaning the clay pebbles, ensuring that all debris is removed, and perform multiple rinses to flush out any remnants of old plant matter. This step is crucial to prevent the accumulation of decaying material over time.
+For effective initial cleaning, solid bottom 1020 grow trays are highly recommended. Start by placing a layer of pebbles at the bottom of the tray and add just enough water to submerge the pebbles. Using a stiff scrub brush, diligently scrub the pebbles, taking advantage of the low water level that allows them to rotate during cleaning.
+After thorough scrubbing, it's time to perform multiple rinses to completely flush out any dirt or materials clogging the pores of the pebbles. This rinsing process works best in a 5-gallon bucket equipped with multiple drainage holes. To facilitate easy drainage, drill holes with diameters ranging from ⅛” to ¼”. Ensure that the bottom of the bucket has several holes, along with additional holes around the sides to maximize drainage efficiency.
+            `,
+    otherImages: [
+      "https://images.pexels.com/photos/305087/pexels-photo-305087.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      "https://images.pexels.com/photos/206847/pexels-photo-206847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ],
+  },
+];
+
+export default blogPosts;
